be0ef48cadf5195817cde01123cff276bb20d389
Nilam Doctor <nilamdoc@gmail.com>
2014-06-11 14:41:46 +0530

    update

--- app/controllers/ExController.php
+++ app/controllers/ExController.php
@@ -54,7 +54,7 @@ class ExController extends \lithium\action\Controller {
 		
 		$user = Session::read('member');
 		$id = $user['_id'];
-		if($id==null){$this->redirect(array('controller'=>'ex','action'=>'dashboard/'));}		
+//		if($id==null){$this->redirect(array('controller'=>'ex','action'=>'dashboard/'));}		
 		$details = Details::find('first',
 			array('conditions'=>array('user_id'=>$id))
 		);
