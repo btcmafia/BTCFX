05a6e2693d1a7872929cdd0a2202e85662804caa
Nilam Doctor <nilamdoc@gmail.com>
2014-08-11 18:12:11 +0530

    tidy

--- app/views/admin/transactions.html.php
+++ app/views/admin/transactions.html.php
@@ -17,7 +17,7 @@
 
 $i = 0;
 foreach ($Details	as $tx){?>
-		<tr <?php if(($i%2)==0){?>style="background-color:#B8FBAC"<?php }else{?>style="background-color:#FEEABA"<?php }?>>
+		<tr <?php if(($i%2)==0){?>style="background-color:#333"<?php }else{?>style="background-color:#666"<?php }?>>
 			<td><?=gmdate('Y-M-d H:i:s',$tx['DateTime']->sec)?></td>
 			<td><a href="/Admin/detail/<?=$tx['username']?>" target="_blank"><?=$tx['username']?></a><br>
 				<span class="label
@@ -52,20 +52,25 @@ foreach ($Details	as $tx){?>
 			<td><?php if($tx['Added']==true){echo "Deposit";}else{echo "Withdraw";}?></td>
 			<td style="text-align:center"><?=$tx['Approved']?></td>			
 			<td>
-			<a href="/admin/sendemailtransaction/<?=$tx['_id']?>" class="tooltip-x label label-success" rel="tooltip-x" data-placement="top" title="Send customer an email to deposit funds in our bank">Send Approval</a>
+			<a href="/admin/sendemailtransaction/<?=$tx['_id']?>" class="tooltip-x label label-success" rel="tooltip-x" data-placement="top" title="Send customer an email to deposit funds in our bank">Send Approval</a><br>
+			
 			<a href="/admin/deletetransaction/<?=$tx['_id']?>" class="tooltip-x label label-warning" rel="tooltip-x" data-placement="top" title="Cannot be recovered">Delete</a>
-			<a href="/admin/rejecttransaction/<?=$tx['_id']?>/D1"  class="tooltip-x label label-important" rel="tooltip-x" data-placement="top" title="User will be sent an email about rejection, cannot be recovered">Reject ></a>			
+			<a href="/admin/rejecttransaction/<?=$tx['_id']?>/D1"  class="tooltip-x label label-important" rel="tooltip-x" data-placement="top" title="User will be sent an email about rejection, cannot be recovered">Reject ></a><br>
+			
 			<select id="RejectReason" class="span2 label label-important" onChange="RejectReason(this.value);">
 			<?php foreach($reasons as $reason){	?>
 				<option value="<?=$reason['code']?>"><?=$reason['reason']?></option>
 			<?php }?>
-			</select>
+			</select><br>
+			
 			<form action="/admin/approvetransaction" method="post" class="form form-horizontal">
 				<input type="text" name="Amount" id="Amount" value="<?=$tx['Amount']?>" max="<?=$tx['Amount']?>" min="1" class="span1 tooltip-x" rel="tooltip-x" data-placement="top" title="Only numbers no comma ">
 				<input type="hidden" name="id" id="id" value="<?=$tx['_id']?>">
 				<input type="hidden" name="Currency" id="Currency" value="<?=$tx['Currency']?>">				
+				<br>
 				<input type="submit" value="Deposit to ibwt" class="btn btn-primary tooltip-x" rel="tooltip-x" data-placement="top" title="Approve this transaction">
 			</form>
+			
 			</td>
 		</tr>
 <?php  foreach($tx['Previous'] as $pt){ ?>
