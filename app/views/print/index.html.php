8321f3c4cb812cd60cda370631ba99963cad9d5a
Nilam Doctor <nilamdoc@gmail.com>
2014-08-23 11:58:59 +0530

    print

--- app/views/print/index.html.php
+++ app/views/print/index.html.php
@@ -1,35 +1,28 @@
-<div style="background-image:url(/img/Stamp.png);background-position:bottom right;background-repeat:no-repeat">
-<div class="row container" >
-	<div class="span12">
-		<div class="navbar">
-			<div class="navbar-inner1">
-				<a class="brand" href="#">Print / Cold storage </a>
-			</div>
-		</div>
-		<table class="table table-condensed table-bordered table-hover" style="margin-top:-20px">
-			<tr>
-			<td>
-				<h5>Uses:</h5>
-				<ol>
-					<li><strong>Security</strong>: As this is an offline virtual currency, it is very safe from the hackers of the internet world.</li>
-					<li><strong>Gift</strong>: You can gift these bitcoins to friends, children, grandchildren and make them aware of this new phenomena. Also make their world secure!</li>
-					<li><strong>Collectibles</strong>: Use them as collectibles as you collect stamps.</li>
-					<li><strong>Transact</strong>: Many non-bitcoiners want bitcoins, but they do not trust it unless they see it and feel it. This is one of the best options for traders who sell BTC off cafes, meeting points. They can show them and give them to the buyer for cash!</li>
-					<li><strong>Denominations</strong>: You wallet can be broken to different denominations of safe currency. If you have 100BTC, you can divide them into:
-					<ul>
-						<li>1 x 50 BTC</li>
-						<li>1 x 20 BTC</li>
-						<li>1 x 10 BTC</li>
-						<li>1 x 5 BTC</li>
-						<li>1 x 2 BTC</li>
-						<li>5 x 1 BTC</li>
-						<li>10 x 0.1 BTC</li>
-						<li>20 x 0.2 BTC</li>
-						<li>10 x 0.5 BTC</li>
-					</ul>
-					These can be used as a normal currency when you need to spend it. You will not expose your main wallet to the world.
-					</li>
-				</ol>
+<h3>Print / Cold Storage</h3>
+<table class="table table-condensed table-bordered table-hover">
+	<tr>
+	<td>
+		<h5>Uses:</h5>
+		<ol>
+			<li><strong>Security</strong>: As this is an offline virtual currency, it is very safe from the hackers of the internet world.</li>
+			<li><strong>Gift</strong>: You can gift these bitcoins to friends, children, grandchildren and make them aware of this new phenomena. Also make their world secure!</li>
+			<li><strong>Collectibles</strong>: Use them as collectibles as you collect stamps.</li>
+			<li><strong>Transact</strong>: Many non-bitcoiners want bitcoins, but they do not trust it unless they see it and feel it. This is one of the best options for traders who sell BTC off cafes, meeting points. They can show them and give them to the buyer for cash!</li>
+			<li><strong>Denominations</strong>: You wallet can be broken to different denominations of safe currency. If you have 100BTC, you can divide them into:
+			<ul>
+				<li>1 x 50 BTC</li>
+				<li>1 x 20 BTC</li>
+				<li>1 x 10 BTC</li>
+				<li>1 x 5 BTC</li>
+				<li>1 x 2 BTC</li>
+				<li>5 x 1 BTC</li>
+				<li>10 x 0.1 BTC</li>
+				<li>20 x 0.2 BTC</li>
+				<li>10 x 0.5 BTC</li>
+			</ul>
+			These can be used as a normal currency when you need to spend it. You will not expose your main wallet to the world.
+			</li>
+		</ol>
 <h5>Sample</h5>
 <img src="/img/bitcoin-ibwt.jpg" width="800" style="padding:5px ">
 <h5>Security</h5>
@@ -38,17 +31,6 @@
 <p>This service is free!</p>
 <h5>How to get them</h5>
 <a href="/print/cold" class="btn btn-primary">Get your print!</a>	
-			</td>
-			</tr>
-		</table>	</div>
-</div><br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-</div>
+	</td>
+	</tr>
+</table>
