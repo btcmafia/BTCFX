d18a05f0f52fc88457391c502559b3571ff1590f
Stephen <gangsta@fx.btc.la>
2015-05-29 18:47:22 -0400

    Created Splash page and removed UsersController

---
+++ app/views/in/splash.html.php
@@ -0,0 +1,11 @@
+<div class="row">
+<div class="col-sm-4 col-sm-offset-1">
+<?php echo $this->_render('element', 'settings/two-factor'); ?>
+</div>
+</div>
+
+<div class="row col-sm-offset-5">
+<a href="/in/accounts/"><big>Skip >></big></a>
+</div>
+
+
